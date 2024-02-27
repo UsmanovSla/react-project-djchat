72c0090c7810268cea2afde27fbc489a8d3df33b
Vjaceslav <usmanov@seznam.cz>
2024-02-27 17:05:33 +0100

    Models created

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "server",
+    "account",
 ]
 
 MIDDLEWARE = [
@@ -106,3 +108,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
